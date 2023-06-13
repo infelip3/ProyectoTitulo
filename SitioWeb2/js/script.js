4633e68856ebde245fd98eb84f7e1be5ed6903e1
infelip3 <92731436+infelip3@users.noreply.github.com>
2023-06-13 17:23:22 -0400

    Add files via upload

---
+++ SitioWeb2/js/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Aquí puedes agregar tu código JavaScript para interactuar con el sitio web
+// y agregar funcionalidades según tus necesidades
+
+// Ejemplo:
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+//   // Realiza alguna acción al hacer clic en el botón
+ });
