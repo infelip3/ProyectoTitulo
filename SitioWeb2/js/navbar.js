854dc24e0f9ad1054b6394ff0089e221421402fc
Guido Solar <guido.solar@afex.cl>
2023-07-01 23:18:16 -0400

    Mejoras de header/navbar, se agrega loader

---
+++ SitioWeb2/js/navbar.js
@@ -0,0 +1,47 @@
+// Construyo navbar
+const navbar = 
+`<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container container-navbar">
+        <!-- Logo -->
+        <a class="navbar-brand" href="#">
+            <img src="./images/logo.png" class="img-fluid" alt="FurryCom">
+        </a>
+        <!-- Boton menu response -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Opciones menú -->
+        <div class="collapse navbar-collapse  d-lg-flex justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="busca-mascota.html">Busca tu mascota</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="historias.html">Historias</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="organizaciones.html">Organizaciones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="quienes-somos.html">Quienes Somos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="reportes.html">Generar Reportes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="reportes-plus.html">ReportesPLUS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.html">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>`;
+
+// Insertamos navbar en header
+const header = document.getElementById('nav-placeholder');
+header.insertAdjacentHTML('afterbegin', navbar);
--- SitioWeb2/js/script.js
+++ SitioWeb2/js/script.js
@@ -1,10 +1,5 @@
-// script.js
-
-// Aquí puedes agregar tu código JavaScript para interactuar con el sitio web
-// y agregar funcionalidades según tus necesidades
-
-// Ejemplo:
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-//   // Realiza alguna acción al hacer clic en el botón
- });
+// Ocultamos loading después de 'x' tiempo
+setTimeout(() => {
+    const loader = document.getElementById('page-loader');
+    loader.classList.add('hide');
+}, 1000);
